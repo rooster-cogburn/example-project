aa251693ff99d9ae4658d6ad5d221d43b87007f2
rooster-cogburn <145505941+rooster-cogburn@users.noreply.github.com>
2023-11-01 05:56:51 +0000

    feat: extract script

--- true_grit/extract.py
+++ true_grit/extract.py
@@ -1,1 +1,25 @@
 """The _extract_ phase of the ETL pipeline."""
+import json
+import os
+from pathlib import Path
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def fetch_a_true_grit(year: int) -> None:
+    """Fetch one of the True Grit movies from the oMDB API.
+
+    Args:
+    ----
+        year (int): The year of the movie to fetch. Must be 1969 or 2010.
+    """
+    data = requests.get(
+        f"http://www.omdbapi.com/?t=True+Grit&y={year}&apikey={os.environ['OMDB_API_KEY']}",
+        timeout=10,
+    ).json()
+
+    with Path(f"data/true_grit_{year}.json").open("w") as f:
+        json.dump(data, f, indent=2)
